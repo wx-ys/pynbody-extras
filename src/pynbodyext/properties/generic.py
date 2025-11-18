81d69f34861fa5f1f95d772b9ee1094fd1202d00
gal3d <2035180080@qq.com>
2025-11-18 20:56:24 +0800

    add generic property

---
+++ src/pynbodyext/properties/generic.py
@@ -0,0 +1,38 @@
+
+
+
+import numpy as np
+from pynbody.analysis.angmom import ang_mom_vec
+from pynbody.array import SimArray
+from pynbody.snapshot import SimSnap
+
+from .base import PropertyBase
+
+
+class AngMomVec(PropertyBase[SimArray]):
+    """Angular momentum vector property"""
+    def __call__(self, sim: SimSnap) -> SimArray:
+        return ang_mom_vec(sim)
+
+
+class KappaRot(PropertyBase[float]):
+    """
+    Calculate the fraction of kinetic energy in ordered rotation.
+
+    Notes
+    -----
+    The kappa_rot parameter is defined as in eq. (1) of Sales et al. (2010) [1]_.
+
+    References
+    ----------
+    .. [1] Sales, L. V., et al. 2010, MNRAS, 409, 1541
+    """
+    def __call__(self, sim: SimSnap) -> float:
+        Krot = np.sum(0.5 * sim["mass"] * (sim["vcxy"] ** 2))
+        K = np.sum(sim["mass"] * sim["ke"])
+        return float(Krot / K)
+
+
+
+
+
