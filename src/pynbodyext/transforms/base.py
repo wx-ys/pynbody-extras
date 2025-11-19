6aeaec26fd5a5225872ce8274a36245e4185dba6
gal3d <2035180080@qq.com>
2025-11-19 22:35:48 +0800

    refactor transform by calculate

--- src/pynbodyext/transforms/base.py
+++ src/pynbodyext/transforms/base.py
@@ -1,8 +1,9 @@
 
+import warnings
 from typing import Generic, TypeVar
 
 from pynbody.snapshot import SimSnap
-from pynbody.transformation import Transformation
+from pynbody.transformation import Transformable, Transformation
 
 from pynbodyext.calculate import CalculatorBase
 
@@ -11,6 +12,33 @@ TTrans = TypeVar("TTrans",bound=Transformation, covariant=True)
 class TransformBase(CalculatorBase[TTrans], Generic[TTrans]):
     """ Simsnap -> TTrans """
 
+    move_all: bool = True
+
     def __call__(self, sim: SimSnap) -> TTrans:
+        trans_obj = None
+        if self._transformation is not None:
+            trans_obj = self._transformation(sim)
+        if self._filter is not None:
+            sim = sim[self._filter]
+        try:
+            result = self.calculate(sim, trans_obj)
+        except Exception as e:
+            if trans_obj is not None and self._revert_transformation:
+                trans_obj.revert()
+            raise e
+        if trans_obj is not None and self._revert_transformation:
+            warnings.warn("Previous transformation merged, not reverting", stacklevel=2)
+        return result
+
+    def get_target(self, sim: SimSnap, previous: Transformation | None = None) -> Transformable:
+        if previous is not None:
+            target = previous
+        elif self.move_all:
+            target = sim.ancestor
+        else:
+            target = sim
+        return target
+
+    def calculate(self, sim: SimSnap, previous: Transformation | None = None) -> TTrans:
         raise NotImplementedError
 
