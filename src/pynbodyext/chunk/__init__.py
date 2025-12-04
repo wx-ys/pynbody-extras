b4e2d8a67e46e65bbf16697327ddc68820bda213
gal3d <2035180080@qq.com>
2025-12-04 16:48:13 +0800

    add func dask array check

--- src/pynbodyext/chunk/__init__.py
+++ src/pynbodyext/chunk/__init__.py
@@ -2,8 +2,20 @@
 from importlib.util import find_spec
 
 DASK_AVAILABLE: bool = False
+_dask_array_type: type | None = None
+
 
 if find_spec("dask") is not None:
     DASK_AVAILABLE = True
+    from dask.array import Array as DaskArray
+    _dask_array_type = DaskArray
+
+
+def is_dask_array(obj: object) -> bool:
+    """Check if an object is a Dask array."""
+    if not DASK_AVAILABLE or _dask_array_type is None:
+        return False
+    return isinstance(obj, _dask_array_type)
+
 
 
