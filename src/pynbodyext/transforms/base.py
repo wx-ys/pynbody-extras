7665de98c045eeda65d96297961cbed6efa22ceb
gal3d <2035180080@qq.com>
2025-12-06 18:34:06 +0800

    add chain transformations

--- src/pynbodyext/transforms/base.py
+++ src/pynbodyext/transforms/base.py
@@ -1,5 +1,5 @@
 
-from typing import Generic, TypeVar
+from typing import Any, Generic, TypeVar, cast
 
 from pynbody.snapshot import SimSnap
 from pynbody.transformation import Transformation
@@ -7,8 +7,11 @@ from pynbody.transformation import Transformation
 from pynbodyext.calculate import CalculatorBase
 from pynbodyext.log import logger
 from pynbodyext.util.perf import PerfStats
+from pynbodyext.util.tracecache import TraceManager
 
 TTrans = TypeVar("TTrans",bound=Transformation, covariant=True)
+
+_LastT = TypeVar("_LastT", bound="TransformBase[Any]")
 
 class TransformBase(CalculatorBase[TTrans], Generic[TTrans]):
     """ Simsnap -> TTrans """
@@ -18,9 +21,10 @@ class TransformBase(CalculatorBase[TTrans], Generic[TTrans]):
 
     def _perform_calculation(self, sim: SimSnap, trans_obj: Transformation | None, stats: PerfStats) -> TTrans:
         try:
-            with stats.step("calculate"):
-                logger.debug("[%s] performing calculation ...", self)
-                result = self.calculate(sim, trans_obj)
+            with TraceManager.trace_phase(self, "calculate"):
+                with stats.step("calculate"):
+                    logger.debug("[%s] performing calculation ...", self)
+                    result = self.calculate(sim, trans_obj)
         except Exception as e:
             if trans_obj is not None and self._revert_transformation:
                     trans_obj.revert()
@@ -45,3 +49,39 @@ class TransformBase(CalculatorBase[TTrans], Generic[TTrans]):
     def calculate(self, sim: SimSnap, previous: Transformation | None = None) -> TTrans:
         raise NotImplementedError
 
+    @classmethod
+    def chain(self, *transforms: *tuple[*tuple["TransformBase[Any]", ...], _LastT]) -> _LastT:
+        return chain_transforms(*transforms)
+
+# ---------- typed TransformChain helper ----------
+
+def chain_transforms(
+    *transforms: *tuple[*tuple[TransformBase[Any], ...], _LastT]
+) -> _LastT:
+    """ Chain multiple TransformBase instances together.
+
+    Parameters
+    ----------
+    *transforms : TransformBase
+        The transformations to chain together.
+
+    Returns
+    -------
+    TransformBase
+        The final transformation in the chain.
+
+    Notes
+    -----
+    Each transformation will be applied in the order they are provided.
+    """
+    if not transforms:
+        raise ValueError("At least one transform must be provided.")
+    prev: TransformBase[Any] | _LastT | None = None
+    for transform in transforms:
+        cur = transform
+        if prev is not None:
+            cur = cur.with_transformation(prev)
+        prev = cur
+
+    assert prev is not None
+    return cast("_LastT", prev)
